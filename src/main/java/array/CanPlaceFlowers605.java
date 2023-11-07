91d1539374875ab42c8652af95360f8601365b81
hetunccc <heunccc@gmail.com>
2023-11-07 19:40:49 +0800

    CanPlaceFlowers605

---
+++ src/main/java/array/CanPlaceFlowers605.java
@@ -0,0 +1,15 @@
+package array;
+
+public class CanPlaceFlowers605 {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0) {
+                if ((i == 0 || flowerbed[i - 1] != 1) && (i == flowerbed.length - 1 || flowerbed[i + 1] != 1)) {
+                    flowerbed[i] = 1;
+                    n--;
+                }
+            }
+        }
+        return n <= 0;
+    }
+}
